59081ea1fcbddb101ef792d35110d88a4096b85f
Johannes Stein <johannesstein@freeze-dev.com>
2015-12-09 02:54:05 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(req) {
+  getFolderContents = function(folder, recursive) {
+    return fs.readdirSync(folder).reduce(function(list, file) {
+      var name = path.join(folder, file);
+      var isDir = fs.statSync(name).isDirectory();
+
+      return list.concat((isDir && recursive) ? getFolderContents(name, recursive) : [name]);
+    }, []);
+  };
+
+  req.context = function(folder, recursive, pattern) {
+    var folderContents = getFolderContents(folder, recursive)
+    .map(function(item) {
+      return './' + item;
+    }).filter(function(item) {
+      // Don't include current file
+      if (item === './' + path.basename(__filename)) {
+        return false;
+      }
+
+      return pattern.test(item);
+    });
+
+    var keys = function() {
+      return folderContents;
+    };
+
+    var returnContext = function returnContext(item) {
+      return require(item);
+    };
+
+    returnContext.keys = keys;
+
+    return returnContext;
+  };
+};
